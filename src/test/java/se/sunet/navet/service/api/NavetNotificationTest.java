7d1f5f336f540a739e3c788418327b76dc75c2d1
Lasse Yledahl <ylle@sunet.se>
2023-09-20 11:25:09 +0200

    add NotificationName to Name

--- src/test/java/se/sunet/navet/service/api/NavetNotificationTest.java
+++ src/test/java/se/sunet/navet/service/api/NavetNotificationTest.java
@@ -118,4 +118,25 @@ public class NavetNotificationTest extends SetupCommon {
         assertEquals(jsonResponse.getPopulationItems().get(0).getSecrecyMark(), "J");
         assertEquals(jsonResponse.getPopulationItems().get(0).ProtectedPopulationRegistration(), "J");
     }
+
+    @Test
+    public void testNINNotificationName() throws Exception {
+        Server server = embeddedServer.getServer();
+        String servletPath = "/personpost/navetnotification";
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(server.getURI()).path(servletPath);
+
+        HashMap<String, String> data = new HashMap<>();
+        data.put("identity_number", TEST_PERSON_NOTIFICATIONNAME_NIN);
+
+        Entity entity = Entity.entity(gson.toJson(data), MediaType.APPLICATION_JSON);
+        Response response = target.request(MediaType.APPLICATION_JSON).post(entity);
+
+        NavetNotification.Response jsonResponse = gson.fromJson(response.readEntity(String.class), NavetNotification.Response.class);
+
+        assertNotNull(jsonResponse.getPopulationItems().get(0).getPersonItem());
+        assertNotNull(jsonResponse.getPopulationItems().get(0).getPersonItem().getName());
+        assertNotNull(jsonResponse.getPopulationItems().get(0).getPersonItem().getName().getNotificationName());
+    }
 }
